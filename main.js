21232376a774ffa6c2f2044f3c37120fb57cf192
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-06-23 01:10:51 +0400

    ..

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-let song = document.getElementById("song");
-let progress = document.getElementById("progress");
-let playIcon = document.getElementById("playIcon");
-
-song.onloadedmetadata = () => {
-    progress.max = song.duration;
-    progress.value = song.currentTime;
-}
-
-function playPause() {
-    if (playIcon.classList.contains("fa-pause")) {
-        song.pause();
-        playIcon.classList.remove("fa-pause");
-        playIcon.classList.add("fa-play");
-    } else {
-        song.play();
-        playIcon.classList.add("fa-pause");
-        playIcon.classList.remove("fa-play");
-    }
-}
-
-song.addEventListener('timeupdate', () => {
-    progress.value = song.currentTime;
-});
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
