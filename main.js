22e3adf3d5b14c54d7fa1d0b4af52a356016e34d
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-06-23 21:14:27 +0400

    Update main.js

--- main.js
+++ main.js
@@ -28,3 +28,31 @@
 // }
 // }
  
+// const obj ={
+//   names:'Ani',
+//   age:21,
+//   address:'Tbilisi'
+// };
+// const {age}=obj;
+// // console.log(age);
+// let array = [1,2,3,4,5,6,7];
+// [a,b,...c]=array;
+// console.log(a,b,...c)
+
+function flowers(flowers){
+switch(flowers){
+  case 'ვარდი':
+console.log('ეს არის ვარდი!');
+break;
+  case 'ყაყაჩო':
+    console.log('კი , ეს ყაყაჩოა!');
+    break;
+  case 'ია':
+    console.log('იააა , კი!')
+    break;
+  default:
+    console.log('არცერთი ყვავილი არ არის ნაპოვნი!')
+
+}
+}
+flowers('ყაყაჩო')
