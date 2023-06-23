4e8ebaa2b2c0efbe0bd984eb4c0a92b4613f181f
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-06-23 20:48:11 +0400

    ...

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+// if(true){
+// console.log('ჭეშმარიტია');
+// if(false){
+//    console.log('შესრულდება');
+// }
+// }
+// else{
+//   console.log('არ შესრულდა!!');
+// }
+
+// const array =[1,2,3,5];
+// for(let i =0;i<array.length;i++){
+//   console.log(array[i])
+// }
+
+// const array =[1,2,3,5];
+// let i=0;
+// while (i < 5){
+//   i++;
+//   console.log(array[i])
+// }
+
+// const array =[[1,2,3,5],[5,6,7],[33,44,5,67]];
+
+// for(let i =0; i<array.length;i++){
+// for(let j= 0;j<array.length;j++){
+// console.log(array[i][j])
+// }
+// }
+ 
